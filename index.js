f96073a92e957a97ad940745fd0390bd4321d744
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2020-03-19 14:28:42 +0530

    Add new entry point

---
+++ index.js
@@ -0,0 +1,5 @@
+// To pre-process the data and output a combined file which has all the values in an agreed upon order
+
+// read the files
+// aggregate the data into a single json file
+// write the aggregated in file
