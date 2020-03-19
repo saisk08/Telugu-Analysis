c1bc99408cb107c3733cb48ee99bb7b6d1bbb3f8
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2020-03-19 19:56:43 +0530

    Make the output colorful

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
+/* eslint-disable no-console */
 // To pre-process the data and output a combined file which has all the values in an agreed upon order
-
+const chalk = require('chalk');
 const { jsonReader, jsonWriter } = require('./utils/js/io');
 const { aggregate } = require('./utils/js/aggregate');
 
+const info = chalk.hex('#0074D9').bold;
+const success = chalk.hex('#2EcC40').bold;
 // Read the files
+console.log(`${info('[INFO]:')} Reading files`);
 const files = jsonReader('Results');
+console.log(`${success('[DONE]:')} Files read`);
+
 // Aggregate the data into a single json file
+console.log(`${info('[INFO]:')} Aggregating data...`);
 const data = aggregate(files);
+console.log(`${success('[DONE]:')} Aggregation done`);
+
 // Write the aggregated in file
-jsonWriter('Combined/data.json', data);
+console.log(`${info('[INFO]:')} Writing to file`);
+jsonWriter('data.json', data);
+console.log(`${success('[DONE]:')} Data written`);
