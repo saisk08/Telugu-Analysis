85b8ac870715366b250076ef59f1e9d23cf1c0dd
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2020-03-19 17:11:11 +0530

    Add main script

--- index.js
+++ index.js
@@ -3,3 +3,8 @@
 // read the files
 // aggregate the data into a single json file
 // write the aggregated in file
+const { readFilesSync } = require('./utils/js/io');
+const { aggregate } = require('./utils/js/aggregate');
+
+const files = readFilesSync('Results');
+aggregate(files);
