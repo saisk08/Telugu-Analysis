4ecba8ada60c8adeb87b41262c70557ca0757e4b
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2020-03-20 14:16:17 +0530

    Call plot after creating profile

--- main.py
+++ main.py
@@ -4,19 +4,21 @@ import sys
 import _init_paths
 from utils.python.basicinfo import basics
 from utils.python.profile import create_profile
+from utils.python.plotter import make_bar_plot
 import os
 
-try:
-    os.mkdir('Profiles')
-except FileExistsError:
-    pass
+# try:
+#     os.mkdir('Profiles')
+# except FileExistsError:
+#     pass
 
 # Get the json data
 json_file = open('data.json')
 data = json.load(json_file)
-# create user profile for each key
-for user in data:
-    basic_info = basics(data[user])
-    create_profile(basic_info)
+# # create user profile for each key
+# for user in data:
+#     basic_info = basics(data[user])
+#     create_profile(basic_info)
 
 # plot bar graph
+make_bar_plot(data)
