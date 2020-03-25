e9230c07a5b525c30c36655d437363352fa0ca0e
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2020-03-25 14:06:20 +0530

    Call readme creator function

--- main.py
+++ main.py
@@ -3,22 +3,24 @@ import json
 import sys
 import _init_paths
 from utils.python.basicinfo import basics
-from utils.python.profile import create_profile
+from utils.python.profile import create_profile, create_readme
 from utils.python.plotter import make_bar_plot
 import os
 
-# try:
-#     os.mkdir('Profiles')
-# except FileExistsError:
-#     pass
+try:
+    os.mkdir('Profiles')
+except FileExistsError:
+    pass
 
 # Get the json data
 json_file = open('data.json')
 data = json.load(json_file)
-# # create user profile for each key
-# for user in data:
-#     basic_info = basics(data[user])
-#     create_profile(basic_info)
+
+# Create user profile for each key
+for user in data:
+    basic_info = basics(data[user])
+    create_profile(basic_info)
+create_readme(data)
 
 # plot bar graph
 make_bar_plot(data)
