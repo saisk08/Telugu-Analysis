1e0f2fa6e38deec75fda921c82e7fd9999b8a9e3
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2020-03-25 15:13:44 +0530

    Add table of contents

--- utils/python/profile.py
+++ utils/python/profile.py
@@ -12,27 +12,27 @@ def flatten(listOfLists):
 
 def create_profile(data):
     # Create file
-    md_file = MdUtils(
+    profile = MdUtils(
         file_name='Profiles/{}-profile'.format(data[0][1]))
 
     # Insert basic information of the participant
-    md_file.new_header(level=1, title='Profile of {}'.format(data[0][1]))
-    md_file.new_header(level=2, title='Participant information')
-    md_file.write('\n')
-    md_file.insert_code(tabulate(data[:-1]))
+    profile.new_header(level=1, title='Profile of {}'.format(data[0][1]))
+    profile.new_header(level=2, title='Participant information')
+    profile.write('\n')
+    profile.insert_code(tabulate(data[:-1]))
 
     # Insert rdm
-    md_file.new_line()
-    md_file.new_header(level=2, title='RDM fo the participant')
-    md_file.new_line()
+    profile.new_line()
+    profile.new_header(level=2, title='RDM fo the participant')
+    profile.new_line()
     rdm = data[-1].tolist()
     for row, char in zip(rdm, chars):
         row.insert(0, char)
     chars.insert(0, ' ')
-    md_file.new_line('{}'.format(tabulate(rdm, chars, tablefmt='github')))
+    profile.new_line('{}'.format(tabulate(rdm, chars, tablefmt='github')))
 
     # End file
-    md_file.create_md_file()
+    profile.create_md_file()
 
 
 def create_readme(data):
@@ -52,4 +52,5 @@ def create_readme(data):
     readme.new_header(level=2, title='Links to indvidual profile')
     for user in data:
         readme.new_line(''' * [{0}]({0}-profile.md)'''.format(user))
+    readme.new_table_of_contents(table_title='Contents')
     readme.create_md_file()
