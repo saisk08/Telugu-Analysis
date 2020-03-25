ba32cec0f8d93b95a0a6c05b64f58efb731d9cb6
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2020-03-25 14:36:17 +0530

    Spwan python process to create profiles

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 // To pre-process the data and output a combined file which has all the values in an agreed upon order
 const chalk = require('chalk');
+const spwan = require('child_process').spawn;
 const { jsonReader, jsonWriter } = require('./utils/js/io');
 const { aggregate } = require('./utils/js/aggregate');
 
@@ -20,3 +21,8 @@ console.log(`${success('[DONE]:')} Aggregation done`);
 console.log(`${info('[INFO]:')} Writing to file`);
 jsonWriter('data.json', data);
 console.log(`${success('[DONE]:')} Data written`);
+
+// Run the python script
+console.log(`${info('[INFO]:')} Run python analysis script`);
+spwan('python', 'main.py');
+console.log(`${success('[DONE]:')} Profiles created and analysis updated`);
