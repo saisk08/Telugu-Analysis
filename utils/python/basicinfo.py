259cfa225de6d7dc70ff4105aeff17016e8cac8b
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2020-04-10 22:12:34 +0530

    Update the information being passed

--- utils/python/basicinfo.py
+++ utils/python/basicinfo.py
@@ -1,9 +1,13 @@
 from scipy.spatial.distance import squareform
+from .telugu import chars_pairs
 
 
 def basics(user, user_data):
-    info = list()
-    info.append(['Reg. no', user])
+    data = dict()
+    data.update({'user', user})
+
+    basic_info = list()
+    basic_info.append(['Unique ID', user])
     telugu_attr = ''
     if user_data['userInfo']['content']['read'] == 'Yes':
         telugu_attr += 'Read '
@@ -14,41 +18,49 @@ def basics(user, user_data):
     if user_data['userInfo']['content']['write'] == 'Yes':
         telugu_attr += 'Write '
 
-    info.append(['Telugu', telugu_attr])
+    basic_info.append(['Telugu', telugu_attr])
 
     for language in user_data['userInfo']['content']['languages']:
-        info.append([language['name'], language['attr']])
+        basic_info.append([language['name'], language['attr']])
 
+    # Get reaction and scores
     ones, twoes, threes, fours, fives = 0, 0, 0, 0, 0
-    reactions = 0
-    values = list()
-
-    for value in user_data['data']:
+    reaction1, reaction2, reaction3, reaction4, reaction5 = 0, 0, 0, 0, 0
+    score_table = list()
+    for value, pair in zip(user_data['data'], chars_pairs):
         if value['value'] is '1' or value['value'] == 'Very dissimilar':
             ones += 1
-            values.append(1)
-        if value['value'] is '2' or value['value'] == 'Dissimilar':
+            reaction1 += value['reactionTime']
+
+        elif value['value'] is '2' or value['value'] == 'Dissimilar':
             twoes += 1
-            values.append(2)
-        if value['value'] is '3' or value['value'] == 'Neutral':
+            reaction2 += value['reactionTime']
+
+        elif value['value'] is '3' or value['value'] == 'Neutral':
             threes += 1
-            values.append(3)
-        if value['value'] is '4' or value['value'] == 'Similar':
+            reaction3 += value['reactionTime']
+
+        elif value['value'] is '4' or value['value'] == 'Similar':
             fours += 1
-            values.append(4)
-        if value['value'] is '5' or value['value'] == 'Very Similar':
+            reaction4 += value['reactionTime']
+
+        elif value['value'] is '5' or value['value'] == 'Very Similar':
             fives += 1
-            values.append(5)
-        reactions += value['reactionTime']
+            reaction5 += value['reactionTime']
 
+        score_table.append([pair, value['value'], value['reactionTime']])
+
+    reactions = reaction1 + reaction2 + reaction3 + reaction4 + reaction5
     reactions /= len(user_data['data'])
+    basic_info.append(['Avg. reaction time(seconds)', reactions])
+    data.update({'info': basic_info})
+    data.update({'scores': score_table})
+    summary = list()
+    summary.append([1, ones, reaction1 / len(reaction1)])
+    summary.append([2, twoes, reaction2 / len(reaction2)])
+    summary.append([3, threes, reaction3 / len(reaction3)])
+    summary.append([4, fours, reaction4 / len(reaction4)])
+    summary.append([5, fives, reaction5 / len(reaction5)])
+    data.update({'summary': summary})
 
-    info.append(['Reaction time(avg)', reactions])
-    info.append(['Ones', ones])
-    info.append(['Twos', twoes])
-    info.append(['Threes', threes])
-    info.append(['Fours', fours])
-    info.append(['Fives', fives])
-    info.append(squareform(values))
-
-    return info
+    return data
