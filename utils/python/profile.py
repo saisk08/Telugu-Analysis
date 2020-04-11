bf0946676778e0319f9b73e1f7ec33d8a9149aa5
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2020-04-11 21:42:11 +0530

    Modify readme creation
    * Inculde more aboyut reaction time
    * Use better formatting

--- utils/python/profile.py
+++ utils/python/profile.py
@@ -1,6 +1,6 @@
 from tabulate import tabulate
 from .telugu import chars_pairs
-from .plotter import preprocess
+from .basicinfo import overall_data
 
 
 def create_profile(data):
@@ -32,23 +32,38 @@ def create_profile(data):
     profile.close()
 
 
-def create_readme(data):
-    counts = preprocess(data)
+def create_readme(user_data):
+    data = overall_data(user_data)
     readme = open('Profiles/README.md')
-
-    readme.new_header(level=1, title='Analysis')
-    readme.new_line(
+    readme.write('# Analysis of experiment-version 1\n')
+    readme.write(
         '''A profile for each participant has been created. In this document,
-        we look at the measures that give an overall look of the data''')
-    readme.new_header(level=2, title='Links to indvidual profile')
-    for user in data:
-        readme.new_line(''' * [{0}]({0}.md)'''.format(user))
-    readme.new_header(level=2, title='Agreement on pairs ratings')
-    for pair, ratings_count in zip(chars_pairs, counts):
-        readme.new_header(level=3, title='**{}**:'.format(pair))
-        for idx, count in enumerate(ratings_count):
-            readme.new_line('Score {}: {}, ({:.2f} %)'.format(
-                idx + 1, count, (count / sum(ratings_count)) * 100))
+        we look at the measures that give an overall description of the data\n\n''')
+    readme.write('## Summary of the participation\n\n')
+    readme.write(tabulate(data['scores'], tablefmt='github', headers='keys'))
+    readme.write('\n')
+    readme.write('## Agreeement percentages and reaction times\n')
+    readme.write('''
+    !!! tip "About agreement percentages"
+        Agreement percentage is defined as the percentage of participants that
+        rated a pair a particular score (`1` or `2` or `3` or `4` or `5`). Therefore, agreement
+        percentage is calculated for each score of each pair.
+    ''')
+    readme.write('\n\n')
+    readme.write('''
+    !!! info
+        * All reaction times are given in seconds.
+        * The mean and standard deviation below are of the reaction times.
+    ''')
+    for pair, info_vec in zip(chars_pairs, data['overall']):
+        readme.write('\n\n')
+        readme.write('### {}\n'.format(pair))
+        head = ['Score', 1, 2, 3, 4, 5]
+        count = ['Count', *info_vec[0]]
+        percentages = ['Agreement %', *info_vec[1]]
+        mean = ['Mean', *info_vec[2]]
+        std = ['Std', *info_vec[3]]
+        readme.write(tabulate([count, percentages, mean,
+                               std], tablefmt='github', headers=head))
 
-    readme.new_table_of_contents(table_title='Contents', depth=2)
-    readme.create_md_file()
+    readme.close()
