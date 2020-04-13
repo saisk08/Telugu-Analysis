4edb2649e9058f971de9ed3c70b3ccdc97ff2676
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2020-04-13 13:02:27 +0530

    Add more information to the readme document
    * Add summary of agreement
    * Re-arrange the structure of the document for more clarity
    * Fix gramatical errors
    * Add more information to make certain aspects clear

--- utils/python/profile.py
+++ utils/python/profile.py
@@ -37,16 +37,18 @@ def create_readme(user_data, version):
     readme.write('# Analysis for version {}\n\n'.format(version))
     readme.write(
         '''A profile for each participant has been created. In this document,
-        we look at the measures that give an overall description of the data\n\n''')
+        we look at measures that give an overall description of the data.\n\n''')
+    readme.write(
+        '**Total number of participants**: {}\n\n'.format(len(user_data)))
     readme.write('## Summary of the participation\n\n')
     readme.write(tabulate(data['scores'], tablefmt='github', headers='keys'))
     readme.write('\n\n')
-    readme.write('## Agreeement percentages and reaction times\n\n')
     readme.write('''
-!!! tip "About agreement percentages"
+!!! tip "About agreement percentages and majority"
     Agreement percentage is defined as the percentage of participants that
     rated a pair a particular score (`1` or `2` or `3` or `4` or `5`). Therefore, agreement
-    percentage is calculated for each score of each pair.
+    percentage is calculated for each score of each pair. If the maximum agreement percentage of
+    a pair is more than 50%, then the pair has a **majority**.
     ''')
     readme.write('\n\n')
     readme.write('''
@@ -54,10 +56,26 @@ def create_readme(user_data, version):
     * All reaction times are given in seconds.
     * The mean and standard deviation below are of the reaction times.
     ''')
+    readme.write('\n\n')
+    readme.write('## Summary of agreement between participants\n\n')
+    readme.write('''
+* Total pairs: 300
+* Percentage in majority: {6:.3f}%
+* Percentage in minority: {7:.3f}%
+* Majority exists: {0}
+* Number of pairs with:
+    * Majority having score of 1: {1}
+    * Majority having score of 2: {2}
+    * Majority having score of 3: {3}
+    * Majority having score of 4: {4}
+    * Majority having score of 5: {5}
+    \n\n'''.format(data['agreement'].sum(), *data['agreement'], (data['agreement'].sum() / 300) * 100,
+                   100 - ((data['agreement'].sum() / 300) * 100)))
+    readme.write('## Agreeement percentages and reaction times\n\n')
     for pair, info_vec in zip(chars_pairs, data['overall']):
         readme.write('\n\n')
         readme.write('### {}\n\n'.format(pair))
-        readme.write('`Median value: {}`\n\n'.format(info_vec[4]))
+        readme.write('* **Median score**: {}\n\n'.format(info_vec[4]))
         head = ['Score', 1, 2, 3, 4, 5]
         count = ['Count', *info_vec[0]]
         percentages = ['Agreement %', *info_vec[1]]
