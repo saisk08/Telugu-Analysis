88a91d2439855afa7f86f19cf2e43d545d9c9044
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2020-04-15 12:41:49 +0530

    Updates

---
+++ js/mathjax.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-undef */
+window.MathJax = {
+  tex2jax: {
+    inlineMath: [['\\(', '\\)']],
+    displayMath: [['\\[', '\\]']],
+  },
+  TeX: {
+    TagSide: 'right',
+    TagIndent: '.8em',
+    MultLineWidth: '85%',
+    equationNumbers: {
+      autoNumber: 'AMS',
+    },
+    unicode: {
+      fonts: "STIXGeneral,'Arial Unicode MS'",
+    },
+  },
+  displayAlign: 'left',
+  showProcessingMessages: false,
+  messageStyle: 'none',
+};
