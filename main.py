9b54d7b5858be8e76ba5ea1568b076004ac78df3
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2020-04-16 19:57:36 +0530

    Update the pipeline

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from utils.python.basicinfo import basics
 from utils.python.profile import create_profile, create_readme
 from utils.python.plotter import make_bar_plot
 from utils.python.create_index import create_index
+from utils.python.maj_min import majority_report, minority_report
 import os
 from tqdm import tqdm
 from termcolor import colored
@@ -22,10 +23,14 @@ for user in tqdm(ver1_data):
     basic_info = basics(user, ver1_data[user])
     create_profile(basic_info, 1)
 create_readme(ver1_data, 1)
+majority_report(ver1_data, 1)
+minority_report(ver1_data, 1)
 
 for user in tqdm(ver2_data):
     basic_info = basics(user, ver2_data[user])
     create_profile(basic_info, 2)
 create_readme(ver2_data, 2)
+majority_report(ver2_data, 2)
+minority_report(ver2_data, 2)
 create_index(ver1_data, ver2_data)
 print(colored('Pages generated', 'green'))
