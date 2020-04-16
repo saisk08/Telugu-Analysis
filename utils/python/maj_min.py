ce93c690b7d42e3be520e27ddccf16e7ed7c7e3d
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2020-04-16 20:10:02 +0530

    Fix errors

--- utils/python/maj_min.py
+++ utils/python/maj_min.py
@@ -7,11 +7,12 @@ from .telugu import chars_pairs
 def minority_report(input_data, version):
     data = overall_data(input_data)
     bins = [[], [], [], [], []]
-    min_report = open('docs/Version-{}/minority_report.md', 'w+')
+    min_report = open(
+        'docs/Version-{}/minority_report.md'.format(version), 'w+')
     min_report.write('# Minority Report\n\n')
     min_report.write('This report consists all the pairs which have their highest \
     agreement percentage, less that 50\%\n\n')
-    min_report.write('# Summary table\n\n')
+    min_report.write('## Summary table\n\n')
     for index, arr in enumerate(data['overall']):
         high = max(arr[1])
         if high <= 10:
@@ -24,11 +25,11 @@ def minority_report(input_data, version):
             bins[3].append(index)
         elif high > 40 and high <= 50:
             bins[4].append(index)
-    table = [['[0, 10]', len(bin[0])],
-             ['(10, 20]', len(bin[1])],
-             ['(20, 30]', len(bin[2])],
-             ['(30, 40]', len(bin[3])],
-             ['(40, 50]', len(bin[4])]]
+    table = [['[0, 10]', len(bins[0])],
+             ['(10, 20]', len(bins[1])],
+             ['(20, 30]', len(bins[2])],
+             ['(30, 40]', len(bins[3])],
+             ['(40, 50]', len(bins[4])]]
     min_report.write(
         tabulate(table, headers=['Range', 'Count'], tablefmt='github'))
     min_report.write('\n\n')
@@ -45,11 +46,12 @@ def minority_report(input_data, version):
 def majority_report(input_data, version):
     data = overall_data(input_data)
     bins = [[], [], [], [], []]
-    min_report = open('docs/Version-{}/majority_report.md', 'w+')
-    min_report.write('# Majority Report\n\n')
-    min_report.write('This report consists all the pairs which have their highest \
+    max_report = open(
+        'docs/Version-{}/majority_report.md'.format(version), 'w+')
+    max_report.write('# Majority Report\n\n')
+    max_report.write('This report consists all the pairs which have their highest \
     agreement percentage, greater that 50\%\n\n')
-    min_report.write('# Summary table\n\n')
+    max_report.write('## Summary table\n\n')
     for index, arr in enumerate(data['overall']):
         high = max(arr[1])
         if high > 50 and high <= 60:
@@ -62,19 +64,19 @@ def majority_report(input_data, version):
             bins[3].append(index)
         elif high > 90 and high <= 100:
             bins[4].append(index)
-    table = [['[50, 60]', len(bin[0])],
-             ['(60, 70]', len(bin[1])],
-             ['(70, 80]', len(bin[2])],
-             ['(80, 90]', len(bin[3])],
-             ['(90, 100]', len(bin[4])]]
-    min_report.write(
+    table = [['[50, 60]', len(bins[0])],
+             ['(60, 70]', len(bins[1])],
+             ['(70, 80]', len(bins[2])],
+             ['(80, 90]', len(bins[3])],
+             ['(90, 100]', len(bins[4])]]
+    max_report.write(
         tabulate(table, headers=['Range', 'Count'], tablefmt='github'))
-    min_report.write('\n\n')
-    min_report.write('## List of pairs\n\n')
+    max_report.write('\n\n')
+    max_report.write('## List of pairs\n\n')
     for indx, pairs in enumerate(bins):
-        min_report.write('### Range {}0-{}0\n\n'.format(indx, indx + 1))
+        max_report.write('### Range {}0-{}0\n\n'.format(indx + 5, indx + 6))
         for pair in pairs:
-            min_report.write('* {}\n'.format(chars_pairs[pair]))
-        min_report.write('\n')
-    min_report.close()
+            max_report.write('* {}\n'.format(chars_pairs[pair]))
+        max_report.write('\n')
+    max_report.close()
     return
